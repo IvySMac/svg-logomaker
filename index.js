950c8d7a5b0eb74dfcefc216714177ec04042b00
Creature Feature <ivy.mccurdy@gmail.com>
2023-05-25 22:42:17 -0500

    passed in name and text color into argument, added text color question, tested code

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ const fs = require("fs");
 
 
 function askQuestions() {
-    inquirer.prompt(questions).then((responses) => {
-        const logo=generateLogo({responses});
+    inquirer.prompt(questions).then(({name, textColor}) => {
+        const logo=generateLogo(name, textColor);
         
         fs.writeFile("logo.svg",logo, (err) =>
             err? console.log(err) : console.log('Success!')
         );
     });
 }
-
-// function askQuestions() {
-//     inquirer.prompt(questions).then((data) => {
-//         const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-//         fs.writeFile(filename, JSON.stringify(data,null, '\t'), (err) =>
-//             err? console.log(err) : console.log('Success!')
-//         );
-//     });
-// }
 
 function main() {
     askQuestions();
