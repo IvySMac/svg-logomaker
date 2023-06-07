3aa23ea50097d8369642be506189217561c2063e
Creature Feature <ivy.mccurdy@gmail.com>
2023-06-06 22:52:29 -0500

    deleted unnecessary code in index file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const {Circle, Triangle, Square}= require('./lib/shapes');
 const { questions } = require("./lib/questions");
-// const generateLogo =require("./lib/generateLogo");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
@@ -34,19 +33,3 @@ function main() {
 main();
 
 
-// function askQuestions() {
-//     inquirer.prompt(questions).then(({name, textColor,shapes, shapeColor}) => {
-//         const logo=generateLogo(name, textColor, shapes, shapeColor);
-//         //else if statement here
-//         //shape=new Triangle
-//         fs.writeFile("logo.svg",logo, (err) =>
-//             err? console.log(err) : console.log('Generated logo.svg')
-//         );
-//     });
-// }
-
-// function main() {
-//     askQuestions();
-// }
-
-// main();
