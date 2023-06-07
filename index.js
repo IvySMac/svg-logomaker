fa0c62a56435bc248598632d8c99a3f28b97ff50
Creature Feature <ivy.mccurdy@gmail.com>
2023-06-06 22:51:03 -0500

    created classes for each shape, re-linked the shapes to the index, added classes to .prompt in index file by creating an if else statement, adjusted questions to align with index and shapes files, did tests to check shapes, adjusted text alignment on shapes

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+const {Circle, Triangle, Square}= require('./lib/shapes');
 const { questions } = require("./lib/questions");
-const generateLogo =require("./lib/generateLogo");
+// const generateLogo =require("./lib/generateLogo");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
 function askQuestions() {
-    inquirer.prompt(questions).then(({name, textColor,shapes, shapeColor}) => {
-        const logo=generateLogo(name, textColor, shapes, shapeColor);
-        
-        fs.writeFile("logo.svg",logo, (err) =>
-            err? console.log(err) : console.log('Generated logo.svg')
-        );
-    });
+inquirer.prompt(questions)
+.then(({color,textColor,shape,text})=> {
+    if (shape==='Circle'){
+        shape=new Circle(text, color, textColor);
+    }
+    else if (shape==='Triangle'){
+        shape=new Triangle(text, color, textColor);
+    }
+    else {
+        shape=new Square(text, color, textColor);
+    }
+return shape.render();
+
+})
+
+.then((templateString)=> {
+    fs.writeFile("logo.svg",templateString, (err) =>
+    err? console.log(err) : console.log('Generated logo.svg')
+    );
+});
 }
 
 function main() {
@@ -19,3 +32,21 @@ function main() {
 }
 
 main();
+
+
+// function askQuestions() {
+//     inquirer.prompt(questions).then(({name, textColor,shapes, shapeColor}) => {
+//         const logo=generateLogo(name, textColor, shapes, shapeColor);
+//         //else if statement here
+//         //shape=new Triangle
+//         fs.writeFile("logo.svg",logo, (err) =>
+//             err? console.log(err) : console.log('Generated logo.svg')
+//         );
+//     });
+// }
+
+// function main() {
+//     askQuestions();
+// }
+
+// main();
